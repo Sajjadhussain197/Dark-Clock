3837f8c2e231f6632a712914eed19d9c481c123d
Sajjad <sajjadhussain9582@gmail.com>
2024-05-31 14:51:54 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const deg=6;
+
+
+
+let hr = document.querySelector("#hr")
+let mn = document.querySelector("#mn")
+let sc = document.querySelector("#sc")
+
+
+setInterval(() => {
+
+    let day = new Date();
+
+    let hh= day.getHours()*30;
+    let mm= day.getMinutes()*deg;
+    let ss = day.getSeconds()*deg;
+
+
+
+    hr.style.transform =`rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform= `rotateZ(${(mm)}deg)`;
+    sc.style.transform = `rotateZ(${(ss)}deg)`
+    
+});
+
